408efd5798cf5b126b68dd53ba680d1db40b2eb4
409H <harry.denley@outlook.com>
2018-06-24 15:50:03 +0100

    Modified icon to the new logo

--- src/esd.js
+++ src/esd.js
@@ -13,35 +13,43 @@ function EtherScamDbValidateAddress()
                 default:
                 case 'neutral':
                     document.getElementById("esd-address-verify").innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:se="http://svg-edit.googlecode.com" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" style="width:50px;height:70px">
-                            <title>This address has not been verified by EtherScamDB</title>
-                            <g style="">
-                                <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.5934143066406" y="6.26373291015625" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" class="" rx="5" ry="5" fill-opacity="1"></rect>
-                                <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.81317138671875" y="25.1648406982422" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" rx="5" ry="5" fill-opacity="1"></rect>
-                                <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.81317138671875" y="44.39559936523438" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" class="" rx="5" ry="5" fill-opacity="1"></rect>
-                                <foreignObject fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" font-size="50" font-family="Georgia, serif" letter-spacing="0" word-spacing="0" marker-start="" marker-mid="" marker-end="" x="8.5164794921875" y="22.4945068359375" width="50" height="57" style="color: rgb(227, 235, 148);" stroke-opacity="1">
-                                    <p xmlns="http://www.w3.org/1999/xhtml" style="border:none;outline:none;font-size:inherit;line-height:1em;padding:0;margin:0;">
-                                        <span style="border:none;outline:none;font-size:inherit;line-height:1em;padding:0;margin:0;-webkit-text-stroke:1px #fff;">⚠</span>
-                                    </p>
-                                </foreignObject>
+                    <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    width="30.555px" height="50px" viewBox="0 0 609.555 1000" enable-background="new 0 0 609.555 1000" xml:space="preserve">
+                        <title>This address has not been verified EtherScamDB</title>
+                        <g>
+                            <g>
+                                <path fill="#505461" d="M9.337,557.712c5.979,20.553,13.911,40.265,23.809,58.8l-0.04,0.023l0.341,0.572
+                                    c4.45,8.303,9.294,16.359,14.473,24.185L304.794,1000V733.475L9.337,557.712z"/>
+                                <path fill="#282138" d="M304.794,733.475V1000l256.875-358.708c5.176-7.825,10.022-15.882,14.473-24.185l0.34-0.572l-0.039-0.023
+                                    c9.895-18.535,17.832-38.247,23.806-58.8L304.794,733.475z"/>
                             </g>
-                        </svg>
+                            <polygon fill="#282138" points="304.776,0 304.776,387.292 476.391,285.198 	"/>
+                            <path fill="#282138" d="M502.419,328.454l-4.897-8.177L396.74,380.242l-91.946,143.716l-0.015,163.935L609.555,506.57
+                                L502.419,328.454z M381.535,559.258l86.699-134.769l34.045,60.231L381.535,559.258z"/>
+                            <polygon fill="#505461" points="304.779,687.893 0,506.57 107.139,328.454 112.036,320.277 212.794,380.225 304.741,523.944 	"/>
+                            <polygon fill="#505461" points="304.776,387.292 304.776,0 133.213,285.237 	"/>
+                        </g>
+                    </svg>
                     `;
                     break;
                 case 'blocked':
                     document.getElementById("esd-address-verify").innerHTML = `
                         <a href="https://etherscamdb.info/address/${addr}" target="_blank">
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:se="http://svg-edit.googlecode.com" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" style="width:50px;height:70px">
+                            <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                        width="30.555px" height="50px" viewBox="0 0 609.555 1000" enable-background="new 0 0 609.555 1000" xml:space="preserve">
                                 <title>This address has been blacklisted by EtherScamDB</title>
-                                <g style="">
-                                    <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.5934143066406" y="6.26373291015625" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" class="" rx="5" ry="5" fill-opacity="1"></rect>
-                                    <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.81317138671875" y="25.1648406982422" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" rx="5" ry="5" fill-opacity="1"></rect>
-                                    <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.81317138671875" y="44.39559936523438" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" class="" rx="5" ry="5" fill-opacity="1"></rect>
-                                    <foreignObject fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" font-size="50" font-family="Georgia, serif" letter-spacing="0" word-spacing="0" marker-start="" marker-mid="" marker-end="" x="14.5164794921875" y="18.4945068359375" width="50" height="57" style="color: rgb(173, 81, 60);" stroke-opacity="1">
-                                        <p xmlns="http://www.w3.org/1999/xhtml" style="border:none;outline:none;font-size:inherit;line-height:1em;padding:0;margin:0;">
-                                            <span style="border:none;outline:none;font-size:48pt;line-height:1em;padding:0;margin:0;-webkit-text-stroke:1px #fff;">⨯</span>
-                                        </p>
-                                    </foreignObject>
+                                <g>
+                                    <g>
+                                        <path fill="#BD1327" d="M9.337,557.712c5.979,20.553,13.911,40.265,23.809,58.8l-0.04,0.023l0.341,0.572
+                                            c4.45,8.303,9.294,16.359,14.473,24.185L304.794,1000V733.475L9.337,557.712z"/>
+                                        <path fill="#801F35" d="M304.794,733.475V1000l256.875-358.708c5.176-7.825,10.022-15.882,14.473-24.185l0.34-0.572l-0.039-0.023
+                                            c9.895-18.535,17.832-38.247,23.806-58.8L304.794,733.475z"/>
+                                    </g>
+                                    <polygon fill="#801F35" points="304.776,0 304.776,387.292 476.391,285.198 	"/>
+                                    <path fill="#801F35" d="M502.419,328.454l-4.897-8.177L396.74,380.242l-91.946,143.716l-0.015,163.935L609.555,506.57
+                                        L502.419,328.454z M381.535,559.258l86.699-134.769l34.045,60.231L381.535,559.258z"/>
+                                    <polygon fill="#BD1327" points="304.779,687.893 0,506.57 107.139,328.454 112.036,320.277 212.794,380.225 304.741,523.944 	"/>
+                                    <polygon fill="#BD1327" points="304.776,387.292 304.776,0 133.213,285.237 	"/>
                                 </g>
                             </svg>
                         </a>
@@ -49,19 +57,23 @@ function EtherScamDbValidateAddress()
                     break;
                 case 'verified':
                     document.getElementById("esd-address-verify").innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:se="http://svg-edit.googlecode.com" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" style="width:50px;height:70px">
-                            <title>This address has been whitelisted by EtherScamDB</title>
-                            <g style="">
-                                <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.5934143066406" y="6.26373291015625" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" class="" rx="5" ry="5" fill-opacity="1"></rect>
-                                <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.81317138671875" y="25.1648406982422" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" rx="5" ry="5" fill-opacity="1"></rect>
-                                <rect fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" x="1.81317138671875" y="44.39559936523438" width="37.71428680419922" height="16.39560317993164" style="color: rgb(0, 0, 0);" class="" rx="5" ry="5" fill-opacity="1"></rect>
-                                <foreignObject fill="#6d6d6d" stroke="#222222" stroke-width="2" stroke-linejoin="round" stroke-dashoffset="" fill-rule="nonzero" font-size="50" font-family="Georgia, serif" letter-spacing="0" word-spacing="0" marker-start="" marker-mid="" marker-end="" x="14.5164794921875" y="22.4945068359375" width="50" height="57" style="color: rgb(60, 173, 85);" stroke-opacity="1">
-                                    <p xmlns="http://www.w3.org/1999/xhtml" style="border:none;outline:none;font-size:inherit;line-height:1em;padding:0;margin:0;">
-                                        <span style="border:none;outline:none;font-size:inherit;line-height:1em;padding:0;margin:0;">✓</span>
-                                    </p>
-                                </foreignObject>
+                    <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                    width="30.555px" height="50px" viewBox="0 0 609.555 1000" enable-background="new 0 0 609.555 1000" xml:space="preserve">
+                        <title>This address has been verified by EtherScamDB</title>
+                        <g>
+                            <g>
+                                <path fill="#1F8027" d="M9.337,557.712c5.979,20.553,13.911,40.265,23.809,58.8l-0.04,0.023l0.341,0.572
+                                    c4.45,8.303,9.294,16.359,14.473,24.185L304.794,1000V733.475L9.337,557.712z"/>
+                                <path fill="#1F8021" d="M304.794,733.475V1000l256.875-358.708c5.176-7.825,10.022-15.882,14.473-24.185l0.34-0.572l-0.039-0.023
+                                    c9.895-18.535,17.832-38.247,23.806-58.8L304.794,733.475z"/>
                             </g>
-                        </svg>
+                            <polygon fill="#1F8021" points="304.776,0 304.776,387.292 476.391,285.198 	"/>
+                            <path fill="#1F8021" d="M502.419,328.454l-4.897-8.177L396.74,380.242l-91.946,143.716l-0.015,163.935L609.555,506.57
+                                L502.419,328.454z M381.535,559.258l86.699-134.769l34.045,60.231L381.535,559.258z"/>
+                            <polygon fill="#1F8027" points="304.779,687.893 0,506.57 107.139,328.454 112.036,320.277 212.794,380.225 304.741,523.944 	"/>
+                            <polygon fill="#1F8027" points="304.776,387.292 304.776,0 133.213,285.237 	"/>
+                        </g>
+                    </svg>
                     `;
                     break;
             }
